8452dceba93bb7fb034366adad1dd8fa1d8e035e
Rodrigo Chaves <chavesfop@gmail.com>
2019-03-16 20:24:18 -0300

    commit inicial

---
+++ trabalho1/Leitor.java
@@ -0,0 +1,16 @@
+package trabalho1;
+
+public class Leitor implements Runnable {
+	private Buffer mensagem;
+
+	public Leitor(Buffer msg) {
+		this.mensagem = msg;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 120; i++){
+			while(mensagem.ler() != 0){ }
+		}
+	}
+}
